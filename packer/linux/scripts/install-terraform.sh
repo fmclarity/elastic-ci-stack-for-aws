3e3b27e2d955d97aa02f33312b48ea81644df59d
Gary Liu <gary@fmclarity.com>
2021-12-15 12:35:05 +1100

    install terraform 1.1.0

---
+++ packer/linux/scripts/install-terraform.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+set -eu -o pipefail
+
+# install terraform latest version
+sudo yum install -y yum-utils
+sudo yum-config-manager --add-repo https://rpm.releases.hashicorp.com/AmazonLinux/hashicorp.repo
+sudo yum -y install terraform
