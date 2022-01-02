4b14c5bb50650d4d6d6e211aecc196319732e4e8
Gary Liu <gary@fmclarity.com>
2022-01-02 23:32:43 +1100

    env var queue must be specified

--- custom_resources/make/buildkite.mk
+++ custom_resources/make/buildkite.mk
@@ -4,6 +4,10 @@ AWS_REGION ?= ap-southeast-2
 # Custom make targets
 sshkey:=/tmp/.buildkite-$(queue)
 
+ifeq ($(queue),)
+  $(error environment variable 'queue' must be specified)
+endif
+
 check-ssh-key:
 	@ key=$$(aws --region $(AWS_REGION) ec2 describe-key-pairs --key-names buildkite-$(queue) --query "KeyPairs[].KeyName" --output text); \
 		if [[ $$key == buildkite-$(queue) ]]; then \
@@ -38,7 +42,7 @@ upload-private-key-buildkite:
 ## Upload buildkite bot user ssh public key to github in order to allow buildkite agent to clone github repos
 ## Note: githubtoken (user buildkite@fmclarity.com PAT) can be retrieved from AWS Secret Manager '/github/api-token/ssh-gpg-management'
 upload-public-key-github:
-	echo "uploading ssh public key to github apt-devops-bk user."
+	echo "uploading ssh public key to github buildkite@fmclarity.com user."
 	githubtoken=$$(aws secretsmanager get-secret-value --secret-id /github/api-token/ssh-gpg-management --query 'SecretString' --output text); \
 	pubkey=$$(cat $(sshkey).pub); \
   curl -v -H "Content-Type: application/json" -H "Authorization: token $$githubtoken" \
