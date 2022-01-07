09d8e657dec4b6ac369305bbf895ed5db8b380b5
Gary Liu <gary@fmclarity.com>
2022-01-07 18:49:40 +1100

    pin terraform version to 1.1.2

--- packer/linux/scripts/install-terraform.sh
+++ packer/linux/scripts/install-terraform.sh
@@ -4,4 +4,4 @@ set -eu -o pipefail
 # install terraform latest version
 sudo yum install -y yum-utils
 sudo yum-config-manager --add-repo https://rpm.releases.hashicorp.com/AmazonLinux/hashicorp.repo
-sudo yum -y install terraform
+sudo yum -y install terraform-1.1.2-1.x86_64
